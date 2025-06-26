cfc23a592d788f196d5c20a6d7e8b937e719e94f
Yuritzi Calixto <ramirezcalixtodiana@gmail.com>
2025-06-25 23:44:19 -0600

    Archivos de ruta

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,6 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function(){
+            Route::group(base_path('routes/instructor.php'));
+            Route::group(base_path('routes/admin.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
